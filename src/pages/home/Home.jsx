2adab4238ec8e1bd5ef14a70442592646d283fe6
Dipu Debnath <dipudebnath966@gmail.com>
2023-11-05 12:29:15 -0600

    gallery

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,13 +1,13 @@
 import Banner from "../../components/Banner";
 // import RoomCard from "../../components/RoomCard";
-// import RoomSlider from "../../components/RoomSlider";
+import RoomSlider from "../../components/RoomSlider";
 
 const Home = () => {
     return (
         <div className="space-y-10">
            <Banner></Banner>
-           {/* <RoomSlider></RoomSlider>
-           <RoomCard></RoomCard> */}
+           <RoomSlider></RoomSlider>
+           {/* <RoomCard></RoomCard> */}
         </div>
     );
 };
